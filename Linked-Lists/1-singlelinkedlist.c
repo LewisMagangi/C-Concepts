14c4bc49fe3881ae767ecff10895c542e157aae1
Liquelaliqour <lewismomanyi34@gmail.com>
2022-05-09 08:32:27 -0700

    COnceptualising LinkedLists

---
+++ Linked-Lists/1-singlelinkedlist.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+	int data;
+	struct node *link;
+};
+
+int main()
+{
+	struct node *head = NULL;
+	struct node *current = NULL;
+
+	head = (struct node*)malloc(sizeof(struct node));
+	head -> data = 45;
+	head -> link = NULL;
+
+	current = malloc(sizeof(struct node));
+	current -> data = 98;
+	current -> link = NULL;
+	head -> link = current;
+	printf("%d\n", current->data);
+
+	return (0);
+}
