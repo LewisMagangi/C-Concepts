ccaaac18e848e02b09df13dd995d857dba2db9d7
Liquelaliqour <lewismomanyi34@gmail.com>
2022-05-09 10:27:03 -0700

    Printing data from a linked list

---
+++ Linked-Lists/4-linkedlistcount.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+	int data;
+	struct node *link;
+};
+
+void count_of_nodes(struct node *head)
+{
+	int count = 0;
+	struct node *ptr;
+
+	if (head == NULL)
+		printf("Linked List is empty");
+	ptr = NULL;
+	ptr = head;
+	while (ptr != NULL)
+	{
+		count++;
+		ptr = ptr->link;
+	}
+	printf("No of nodes: %d\n", count);
+}
+int main()
+{
+	struct node *head = NULL;
+	struct node *current = NULL;
+	struct node *current2 = NULL;
+
+	head = (struct node*)malloc(sizeof(struct node));
+	head -> data = 45;
+	head -> link = NULL;
+
+	current = malloc(sizeof(struct node));
+	current -> data = 98;
+	current -> link = NULL;
+	head -> link = current;
+
+	current2 = (struct node*)malloc(sizeof(struct node));
+	current2 -> data = 3;
+	current2 -> link = NULL;
+	current -> link = current2;
+	printf("Current->data: %d\n", current->data);
+	count_of_nodes(head);
+	return (0);
+}
