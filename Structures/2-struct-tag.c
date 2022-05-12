72dea9c2049bf5e5a3196fc6fc2c8c3c4b75bce1
Liquelaliqour <lewismomanyi34@gmail.com>
2022-05-12 06:46:08 -0700

    A more efficient struct tag

---
+++ Structures/2-struct-tag.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+/**
+ *struct car- a new type struct car
+ *@age: The age of the employee
+ *@name: The name of the employee
+ *@salary: The employee's salary
+ */
+struct employee{
+	char *name;
+	int age;
+	int salary;
+};
+
+int manager()
+{
+	struct employee manager;
+	manager.salary = 65000;
+
+	return (manager.salary);
+}
+int main()
+{
+	struct employee emp1;
+	struct employee emp2;
+
+	emp1.salary = 50000;
+	emp2.salary = 35000;
+
+	printf("Manager's Salary: %d\n", manager());
+	printf("1st Employer's Salary: %d\n", emp1.salary);
+	printf("2nd Employer's Salary: %d\n", emp2.salary);
+	return (0);
+}
