4e9d1a37c17f9ae68a2fcb356500d8d3c69d49b1
Liquelaliqour <lewismomanyi34@gmail.com>
2022-12-25 05:38:16 -0800

    hashtables

--- Hash-Tables/0.Def-Hashtable.c
+++ Hash-Tables/0.Def-Hashtable.c
@@ -5,11 +5,10 @@ typedef struct Ht_item Ht_item;
 
 // Define the Hash Table Item here
 struct Ht_item {
-        char* key;
-        char* value;
+  char* key;
+  char* value;
 };
 int main()
 {
   return 0;
 }
-
