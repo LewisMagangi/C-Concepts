29850c83539393894b0d4ce81ea5a2b0a10d097e
Liquelaliqour <lewismomanyi34@gmail.com>
2022-12-25 05:21:08 -0800

    defining a hash table

---
+++ Hash-Tables/0.Def-Hashtable.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Ht_item Ht_item;
+
+// Define the Hash Table Item here
+struct Ht_item {
+        char* key;
+        char* value;
+};
+int main()
+{
+  return 0;
+}
+
