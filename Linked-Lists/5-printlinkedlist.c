b5c754b1548d17b8037f4a070add9ecbb299dfc2
Liquelaliqour <lewismomanyi34@gmail.com>
2022-12-26 04:53:51 -0800

    making it more effecient

--- Linked-Lists/5-printlinkedlist.c
+++ Linked-Lists/5-printlinkedlist.c
@@ -1,10 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-struct node {
-	int data;
-	struct node *link;
-};
+#include "linkedlist.h"
 
 void print_data(struct node *head)
 {
@@ -16,30 +12,7 @@ void print_data(struct node *head)
 	ptr = head;
 	while (ptr != NULL)
 	{
-		printf("prt->data: %d\n", ptr->data);
+		printf("\nprt -> data: %d\n", ptr -> data);
 		ptr = ptr -> link;
 	}
-}
-int main()
-{
-	struct node *head = NULL;
-	struct node *current = NULL;
-	struct node *current2 = NULL;
-
-	head = (struct node*)malloc(sizeof(struct node));
-	head -> data = 45;
-	head -> link = NULL;
-
-	current = malloc(sizeof(struct node));
-	current -> data = 98;
-	current -> link = NULL;
-	head -> link = current;
-
-	current2 = (struct node*)malloc(sizeof(struct node));
-	current2 -> data = 3;
-	current2 -> link = NULL;
-	current -> link = current2;
-	printf("Current->data: %d\n", current->data);
-	print_data(head);
-	return (0);
 }
