afb98930f11dffebdf2bc5c9bdd13d0ddb05794c
Liquelaliqour <lewismomanyi34@gmail.com>
2022-12-29 04:48:47 -0800

    better add data function implementation

--- Linked-Lists/7-add_firstnode.c
+++ Linked-Lists/7-add_firstnode.c
@@ -12,33 +12,37 @@ node_t* add_firstnode(node_t* head, int d)
 	head = ptr;
 	return (head);
 }
+
 int main()
 {
-	struct node *head = NULL;
-	struct node *current = NULL;
-	struct node *current2 = NULL;
-	struct node *ptr = NULL;
-
-	head = (struct node*)malloc(sizeof(struct node));
+        node_t *head = NULL;
+	node_t *current = NULL;
+	node_t *current2 = NULL;
+	
+	head = (node_t*)malloc(sizeof(node_t));
 	head -> data = 45;
 	head -> link = NULL;
-
-	current = (struct node*)malloc(sizeof(struct node));
+	
+	current = malloc(sizeof(node_t));
 	current -> data = 98;
 	current -> link = NULL;
 	head -> link = current;
 
-	current2 = (struct node*)malloc(sizeof(struct node));
+	current2 = (node_t*)malloc(sizeof(node_t));
 	current2 -> data = 3;
 	current2 -> link = NULL;
 	current -> link = current2;
-
-	ptr = (struct node*)malloc(sizeof(struct node));
-	ptr = head;
-	head = add_firstnode(head, 2);
-	head -> link = ptr;
-	add_lastnode(head, 67);
-	add_lastnode(head, 82);
+	
+	printf("Before adding a node at the linked list : ");
 	print_data(head);
+
+	head = add_firstnode(head, 67);
+	printf("After adding a first node at the linked list : ");
+	print_data(head);
+
+	head = add_firstnode(head, 82);
+	printf("After adding another first node at the linked list : ");
+	print_data(head);
+
 	return (0);
 }
--- Linked-Lists/9-add_firstdata.c
+++ Linked-Lists/9-add_firstdata.c
@@ -1,42 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "linkedlist.h"
 
-struct node {
-	int data;
-	struct node *link;
-};
-
-void print_data(struct node *head)
+void add_firstdata(node_t **head, int d)
 {
-	struct node *ptr;
-
-	if (head == NULL)
-		printf("Linked List is empty");
-	ptr = NULL;
-	ptr = head;
-	while (ptr != NULL)
-	{
-		printf("prt->data: %d\n", ptr->data);
-		ptr = ptr -> link;
-	}
-}
-void add_lastnode(struct node *head, int data)
-{
-	struct node *ptr, *temp;
-
-	ptr = head;
-	temp = (struct node*)malloc(sizeof(struct node));
-
-	temp -> data = data;
-	temp -> link = NULL;
-
-	while(ptr -> link != NULL)
-		ptr = ptr -> link;
-	ptr -> link = temp;
-}
-void add_firstdata(struct node **head, int d)
-{
-        struct node *ptr = malloc(sizeof(struct node));
+        node_t *ptr = malloc(sizeof(node_t));
 
 	ptr -> data = d;
 	ptr -> link = NULL;
@@ -49,43 +17,37 @@ void add_firstdata(struct node **head, int d)
 		  ptr = ptr -> link;
 	  }
 }
-void add_firstnode(struct node **head, int d)
-{
-	struct node *ptr = malloc(sizeof(struct node));
-	ptr -> data = d;
-	ptr -> link = NULL;
 
-	ptr -> link = *head;
-	*head = ptr;
-}
 int main()
 {
-	struct node *head = NULL;
-	struct node *current = NULL;
-	struct node *current2 = NULL;
-	struct node *ptr = NULL;
+        node_t *head = NULL;
+        node_t *current = NULL;
+	node_t *current2 = NULL;
 
-	head = (struct node*)malloc(sizeof(struct node));
+	head = (node_t*)malloc(sizeof(node_t));
 	head -> data = 45;
 	head -> link = NULL;
 
-	current = (struct node*)malloc(sizeof(struct node));
+	current = malloc(sizeof(node_t));
 	current -> data = 98;
 	current -> link = NULL;
 	head -> link = current;
-
-	current2 = (struct node*)malloc(sizeof(struct node));
+	
+	current2 = (node_t*)malloc(sizeof(node_t));
 	current2 -> data = 3;
 	current2 -> link = NULL;
 	current -> link = current2;
-
-	ptr = (struct node*)malloc(sizeof(struct node));
-	ptr = head;
-	add_firstnode(&head, 2);
-	head -> link = ptr;
-	add_lastnode(head, 67);
-	add_lastnode(head, 82);
-	add_firstdata(&head, 576);
+	
+	printf("Before adding data in the linked list : ");
 	print_data(head);
+	
+	add_firstdata(&head, 67);
+	printf("After adding data in the linked list : ");
+	print_data(head);
+	
+	add_firstdata(&head, 82);
+	printf("After adding data in the linked list : ");
+	print_data(head);
+	
 	return (0);
 }
