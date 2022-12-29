cb80f76ec8c17e2d0a5c37ae6ec938f8b6477d7a
Liquelaliqour <lewismomanyi34@gmail.com>
2022-12-29 06:32:04 -0800

    a better delete first node function

--- Linked-Lists/13-del_lastnode.c
+++ Linked-Lists/13-del_lastnode.c
@@ -1,61 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "linkedlist.h"
 
-struct node {
-	int data;
-	struct node *link;
-};
-
-void print_data(struct node *head)
-{
-	struct node *ptr;
-
-	if (head == NULL)
-		printf("Linked List is empty");
-	ptr = NULL;
-	ptr = head;
-	while (ptr != NULL)
-	{
-		printf("\nprt -> data: %d\n", ptr->data);
-		ptr = ptr -> link;
-	}
-}
-
-struct node* add_firstnode(struct node* head, int d)
-{
-        struct node *ptr = malloc(sizeof(struct node));
-	ptr -> data = d;
-	ptr -> link = NULL;
-	
-	ptr -> link = head;
-	head = ptr;
-	return (head);
-}
-
-struct node* add_node_at_pos(struct node *head, int data, int pos)
-{
-        struct node *ptr = head;
-        struct node *ptr2 = malloc(sizeof(struct node));
-	ptr2 -> data = data;
-	ptr2 -> link = NULL;
-
-	if (pos == 1)
-	          add_firstnode(head, data);
-	else
-	  {
-	          pos --;
-		  while(pos != 1)
-		    {
-		            ptr = ptr -> link;
-			    pos--;
-		    }
-		  ptr2 -> link = ptr -> link;
-		  ptr -> link = ptr2;
-		  
-	  }
-	return head;
-}
-struct node* del_lastnode(struct node* head)
+node_t* del_lastnode(node_t* head)
 {
         if (head == NULL)
 	         printf("Linked List is empty");
@@ -66,8 +13,8 @@ struct node* del_lastnode(struct node* head)
 	  }
 	else
 	  {
-	         struct node *temp = head;
-		 struct node *temp2 = head;
+	         node_t *temp = head;
+		 node_t *temp2 = head;
 		 while(temp -> link != NULL)
 		   {
 		     temp2 = temp;
@@ -87,27 +34,18 @@ int main()
 	struct node *ptr = NULL;
 
 	head = (struct node*)malloc(sizeof(struct node));
-	head -> data = 45;
+	head -> data = 972;
 	head -> link = NULL;
 
 	current = (struct node*)malloc(sizeof(struct node));
-	current -> data = 98;
+	current -> data = 084;
 	current -> link = NULL;
 	head -> link = current;
 
 	current2 = (struct node*)malloc(sizeof(struct node));
-	current2 -> data = 3;
+	current2 -> data = 869;
 	current2 -> link = NULL;
 	current -> link = current2;
-
-	int data = 69, pos = 2;
-	add_node_at_pos(head, data, pos);
-
-	while (ptr != NULL)
-	  {
-	          printf("prt -> data: %d\n", ptr -> data);
-		  ptr = ptr -> link;
-	  }
 
 	printf("Before deleting the last node: ");
 	print_data(head);
